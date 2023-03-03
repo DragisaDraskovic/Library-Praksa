21d27b961f8b7991ae63f95149b706c1aec2c851
DragisaDraskovic <dragisadrsakovic2@gmail.com>
2023-03-03 11:29:53 +0100

    Start layout

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from '../buttons/Button'
+
+const Footer = () => {
+  return (
+    <div>
+      <Button name='nesto'/>
+    </div>
+  )
+}
+
+export default Footer
