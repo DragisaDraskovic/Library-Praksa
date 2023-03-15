c6ba8bac4a5b4b35bb6e8139796b02340f23ee16
DragisaDraskovic <dragisadrsakovic2@gmail.com>
2023-03-15 11:21:41 +0100

    Route

--- src/components/bookList/BookList.tsx
+++ src/components/bookList/BookList.tsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react'
 
+import InfiniteScroll from 'react-infinite-scroll-component'
+
 import  Book from '../../model/Book'
 import BookService from '../../services/BookService'
 import Card from '../card/Card'
 import './BookList.css'
 
-
 const BookList = () => {
   const [ book, setBook ] = useState<Book[]>([])
+  const [ hasMore, setHasMore ] = useState(true)
+  const [ page, setPage ] = useState(1)
 
   useEffect(() => {
     dataBooks()
-  }, [])
+  }, [ ])
+
+  const handler = () => {
+    setPage(prev => prev + 1)
+  }
 
   const dataBooks = async () => {
     try{
@@ -22,13 +29,24 @@ const BookList = () => {
     }
   }
   return (
-    <div className='container_for_bookList'>
-      {book.length === 0
-        ?
-        ( <h2 className='no_available_message'>No available books</h2> )
-        :
-        (book && book.map((books) => <Card key={books.Id} book={books} />)
-        )}
+    <div>
+      <InfiniteScroll
+        dataLength={book.length}
+        next={handler}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={<b>You have seen all books</b>}
+        scrollThreshold='75%'
+      >
+        <div className='container_for_bookList'>
+          {book.length === 0
+            ?
+            ( <h2 className='no_available_message'>No available books</h2> )
+            :
+            (book && book.map((books) => <Card key={books.Id} book={books} />)
+            )}
+        </div>
+      </InfiniteScroll>
     </div>
   )
 }
