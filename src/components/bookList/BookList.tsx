33c539c45c029a98d8253f7fbd70592b724f03a1
DragisaDraskovic <dragisadrsakovic2@gmail.com>
2023-03-16 20:30:30 +0100

    working search...

--- src/components/bookList/BookList.tsx
+++ src/components/bookList/BookList.tsx
@@ -11,42 +11,50 @@ import Search from '../search/Search'
 import Where from '../../model/Where'
 
 
-interface BooksProps {
-  PageNumber: number,
-  PageLength: number,
-  Search: string
-  Filter: Where[]
-  Sort: string[]
+// interface BooksProps {
+//   pageNumber: number,
+//   pageLength: number,
+//   search: string
+//   filter: Where[]
+//   sort: string[]
+// }
+
+//Move to model
+interface Pagination {
+  size: number,
+  page: number,
+  totalElements: number | null
+}
+
+const initalPagination: Pagination = {
+  size: 20,
+  page: 1,
+  totalElements: null
 }
 
 const BookList = () => {
   const [ book, setBook ] = useState<Book[]>([])
   const [ hasMore, setHasMore ] = useState(true)
   const [ pageNumber, setPageNumber ] = useState(1)
+  const [ pagination , setPagination] = useState<Pagination>(initalPagination);
   const [ searchInput, setSearchInput ] = useState('')
   const [ search, setSearch ] = useState('')
   const [ filter, setFilter ] = useState<Where[]>([])
   const [ sort, setSort ] = useState<string[]>([])
-  const numberCard = 12
 
   const nexPage = () => {
     setPageNumber( pageNumber + 1)
   }
 
-  const dataBooks = async (
-    pageNumber: number,
-    pageLength: number,
-    search: string,
-    filter: Where[],
-    sort: string[]
-  ) => {
-    try{
-      const response = await BookService.getAllBooks({ pageNumber, pageLength, search, filter, sort })
-      setHasMore(pageNumber * numberCard <= response.data.TotalCount)
-      setBook(() => [ ...book,...response.data.Items ])
-    } catch(error) {
-      //console.error(error)
-      setBook([])
+  const dataBooks = ( searchValue: string ) => {
+      const response = BookService.getAllBooks({ pageNumber: pagination.size, pageLength: pagination.page, search: searchValue, })
+      .then((response) => {
+        const totalElements = response.data.TotalCount;
+        setHasMore(pageNumber * totalElements <= totalElements)
+        setBook(() => [ ...book,...response.data.Items ])
+        setPagination((prevState) => { return { ...prevState, totalElements: totalElements }})
+      })
+      .catch((e) => console.log(e))
     }
   }
 
@@ -71,7 +79,7 @@ const BookList = () => {
   )
   return (
     <div>
-      <Search setSearchValue={setSearchInput}/>
+      <Search dataBooks={dataBooks} />
       <InfiniteScroll
         dataLength={book.length}
         next={nexPage}
